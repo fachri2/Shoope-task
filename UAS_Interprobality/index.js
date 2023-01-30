1c3a6111a427773904cd3bf66739b176c3d79d3a
fachri2 <97153863+fachri2@users.noreply.github.com>
2023-01-30 15:54:33 +0700

    Add files via upload

---
+++ UAS_Interprobality/index.js
@@ -0,0 +1,11 @@
+
+// Required modules
+const http = require('http');
+const util = require('util');
+
+// https://github.com/node-formidable/node-formidable
+const Formidable = require('formidable');
+
+//https://www.npmjs.com/package/dotenv
+const cloudinary = require("cloudinary");
+require('dotenv').config()
